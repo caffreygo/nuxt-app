3768a2246565dc86d1d880dc7811ba47b33df496
Jinrui Chen <jinrui@kooboo.cn>
2023-03-04 18:45:20 +0800

    global css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <nav>导航栏</nav>
+    <nav>
+      <img class="avatar" src="~/assets/avatar.jpeg" alt="" />
+      Hello World
+    </nav>
     <slot />
   </div>
 </template>
+
+<style scoped>
+.avatar {
+  width: 50px;
+  border: 1px solid rgb(218, 218, 218);
+  border-radius: 50%;
+}
+</style>
