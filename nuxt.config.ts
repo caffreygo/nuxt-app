d2574def4ed1a2b723c347b9dbd9c7aa1e29c95d
Jinrui Chen <jinrui@kooboo.cn>
2023-03-04 19:14:39 +0800

    global scss & common variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ["~/assets/style/global.css"],
+  css: ["~/assets/style/global.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/assets/style/_variables.scss" as *;`,
+        },
+      },
+    },
+  },
 });
--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -2,4 +2,11 @@
   <h1>
     {{ $route.params.id }}
   </h1>
+  <p>Hello, this is content</p>
 </template>
+
+<style lang="scss" scoped>
+p {
+  color: $linkColor;
+}
+</style>
