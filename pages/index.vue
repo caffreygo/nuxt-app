5b09d7eac901af0383376b7ace6d4b4ae8f38a08
jinrui-kooboo <jinrui@kooboo.cn>
2023-03-08 09:32:02 +0800

    naive-ui-nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,9 @@
 <template>
-  <h1>index page 测试</h1>
-  <NuxtLink to="/detail/1">Detail Page</NuxtLink>
+  <div class="flex items-center flex-col gap-2">
+    <h1>Index Page</h1>
+    <div>
+      <NButton>hello</NButton>
+    </div>
+    <NuxtLink to="/detail/1">Detail Page</NuxtLink>
+  </div>
 </template>
