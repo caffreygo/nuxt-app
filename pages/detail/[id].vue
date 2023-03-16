e4bbbc2a07780a942eefa034bbc0ac60d4123088
jinrui-kooboo <jinrui@kooboo.cn>
2023-03-16 10:32:30 +0800

    throw createError | showError | NuxtErrorBoundary

--- pages/detail/[id].vue
+++ pages/detail/[id].vue
@@ -2,25 +2,33 @@
   <!-- <h1>Detail Page</h1> -->
   <!-- <p>{{ $route.params.id }}</p> -->
   <div class="p-5">
-    <div v-if="pending">加载中...</div>
+    <div v-if="error">{{ errorMsg }}</div>
+    <div v-else-if="pending">加载中...</div>
     <div v-else>
       <h1>{{ data?.title }}</h1>
       <div v-html="data?.content"></div>
       <!-- 评论区 -->
       <div class="py-2">
-        <NInput v-model:value="comment" type="textarea" placeholder="输入评论" />
+        <NInput v-model:value="value" type="textarea" placeholder="输入评论" />
         <NButton @click="onSubmit">发送</NButton>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
+import { NuxtError } from "#app";
 // 获取文章id
 const route = useRoute();
 // const { title, content } = await $fetch(`/api/detail/${route.params.id}`)
 const fetchPost = () => $fetch(`/api/detail/${route.params.id}`);
-const { data, pending } = await useAsyncData(fetchPost);
-
+const { data, pending, error } = await useAsyncData(fetchPost);
+const errorMsg = computed(() => error.value as NuxtError);
+watchEffect(() => {
+  if (unref(error)) {
+    // 如果有错误对象，则展示错误页
+    showError(errorMsg.value);
+  }
+});
 // 请求体
 // $fetch('/api/detail/', {
 //   body: {
@@ -29,14 +37,14 @@ const { data, pending } = await useAsyncData(fetchPost);
 // })
 
 // 增加评论相关逻辑，注意登录状态的获取和使用
-const comment = useState("comment", () => "");
+const value = useState("comment", () => "");
 const store = useUser();
 const { isLogin } = storeToRefs(store);
 const router = useRouter();
 const onSubmit = () => {
   if (isLogin.value) {
     // 提交留言...
-    comment.value = "";
+    value.value = "";
   } else {
     // 要求登录
     router.push("/login?callback=" + route.path);
--- server/api/detail/[id].ts
+++ server/api/detail/[id].ts
@@ -9,7 +9,6 @@ const postsDir = path.join(process.cwd(), "content");
 
 export default defineEventHandler(async (event) => {
   // url路由参数
-  console.log(getRouterParam(event, "id"));
   const fileName = getRouterParam(event, "id") + ".md";
 
   // 获取请求体参数
@@ -21,17 +20,32 @@ export default defineEventHandler(async (event) => {
 
   // 获取文章内容
   const fullPath = path.join(postsDir, fileName);
-  const fileContent = fs.readFileSync(fullPath, "utf-8");
 
-  // 解析扉页信息
-  const matterInfo = matter(fileContent);
+  try {
+    fs.accessSync(fullPath);
+    const fileContent = fs.readFileSync(fullPath, "utf-8");
 
-  // 转换markdown为HTML
-  const processedContent = await remark().use(html).process(matterInfo.content);
-  const content = processedContent.toString();
+    // 解析扉页信息
+    const matterInfo = matter(fileContent);
 
-  return {
-    title: matterInfo.data.title,
-    content,
-  };
+    // 转换markdown为HTML
+    const processedContent = await remark()
+      .use(html)
+      .process(matterInfo.content);
+    const content = processedContent.toString();
+
+    return {
+      title: matterInfo.data.title,
+      content,
+    };
+  } catch (error) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "article is not exist",
+    });
+    // return sendError(event, createError({
+    //   statusCode: 404,
+    //   statusMessage: '文章不存在'
+    // }))
+  }
 });
